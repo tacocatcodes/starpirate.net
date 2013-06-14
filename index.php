0e9bcc820f5082b828b9852731f2d1c2cec186ac
Lane Farrow <lane@Lanes-MacBook-Pro.local>
2013-06-13 20:58:49 -0500

    Initial repository creation.

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+echo 'Click butan to declairs war!<br>';
+
+echo '<form action="submit.php" method="POST"><input type="submit"></form>';
+?>
---
+++ submit.php
@@ -0,0 +1,4 @@
+<?php
+
+echo 'U CLIKED THE BUTAN!';
+?>
